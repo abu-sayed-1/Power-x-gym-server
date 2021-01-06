02004d70a0f23a79cbdc0586e2eb53667b667ffd
as ahmed <rakibasrr13@gmail.com>
2021-01-06 22:05:06 +0300

    power-x-server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+
+const uri = `mongodb+srv://<${process.env.DB_USER}>:<${process.env.DB_PASS}>@cluster0.uj2jz.mongodb.net/<${process.env.DB_NAME}>?retryWrites=true&w=majority`;
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collections = client.db("power-x_gym").collection("homePageAllData");
+   
+    app.post('/homePageData', (req, res) => {
+        console.log(req)
+        const homeData = req.body;
+         collections.insertMany(homeData)
+            .then(result => {
+                console.log(result)
+                // res.send(result.insertedCount > 0)
+                // console.log(result, 'my result..')
+            })
+    })
+
+});
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(4000); 
