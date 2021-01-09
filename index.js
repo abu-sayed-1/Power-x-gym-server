159ebec24e0d19285172510005828be0b2a68170
as ahmed <rakibasrr13@gmail.com>
2021-01-09 15:56:36 +0300

    get home page data

--- index.js
+++ index.js
@@ -4,31 +4,44 @@ const bodyParser = require("body-parser");
 const MongoClient = require('mongodb').MongoClient;
 require("dotenv").config();
 
-const uri = `mongodb+srv://<${process.env.DB_USER}>:<${process.env.DB_PASS}>@cluster0.uj2jz.mongodb.net/<${process.env.DB_NAME}>?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj2jz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collections = client.db("power-x_gym").collection("homePageAllData");
-
+    const homePageDataCollections = client.db("power-x_gym").collection("homePageAllData");
     app.post('/homePageData', (req, res) => {
-        console.log(req)
         const homeData = req.body;
-        collections.insertMany(homeData)
+        homePageDataCollections.insertMany(homeData)
             .then(result => {
-                console.log(result)
                 res.send(result.insertedCount > 0)
             })
             .catch(function (err) {
-                res.status(data.status).send({ message: err.message });
+                res.send({ message: err.message });
             });
+    });
 
+    app.get('/workOutData', (req, res) => {
+        homePageDataCollections.find({}).limit(3)
+            .toArray((err, document) => {
+                res.send(document)
+            })
+    });
+
+    app.get('/ourDream', (req, res) => {
+        const ourDreamData = req.query;
+        console.log(req)
+        homePageDataCollections.find({}).limit(3,4)
+        .toArray((err, document) => {
+            res.send(document)
+        })
     })
 
 });
+
 
 app.get('/', (req, res) => {
     res.send('hello world')
